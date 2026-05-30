9445ee999a49cffc8763503806fb1ab2d7145ce8
agustinIllescasDev <agustinillescas2025@gmail.com>
2026-05-30 02:50:22 -0300

    feat: ajustar longitud de administrador e inicializar producción con un usuario admin

--- src/entities/administrador.entity.ts
+++ src/entities/administrador.entity.ts
@@ -1,18 +1,24 @@
 //administrador.entity.ts
 
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm" 
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity('administradores')
 export class Administrador {
-    @PrimaryGeneratedColumn({name: 'id_administrador'})
-    id_administrador: number
+  @PrimaryGeneratedColumn({ name: 'id_administrador' })
+  id_administrador: number;
 
-    @Column({name: 'nombre', type:'varchar',length:20, nullable: false})
-    nombre: string;
+  @Column({ name: 'nombre', type: 'varchar', length: 50, nullable: false })
+  nombre: string;
 
-    @Column({name: 'mail', type:'varchar',length:100, nullable: false, unique:true})
-    mail: string;
+  @Column({
+    name: 'mail',
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+    unique: true,
+  })
+  mail: string;
 
-    @Column({name: 'password', type:'varchar',length:255, nullable: false})
-    password: string;
+  @Column({ name: 'password', type: 'varchar', length: 255, nullable: false })
+  password: string;
 }
--- src/modules/administradores/administradores.service.ts
+++ src/modules/administradores/administradores.service.ts
@@ -1,106 +1,145 @@
 //administradores.service.ts
 
-import { InjectRepository } from "@nestjs/typeorm";
-import {Administrador} from "src/entities/administrador.entity"
-import {Repository} from "typeorm"
-import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
-import { CreateAdministradorDto } from "./dto/create-administrador.dto";
+import { InjectRepository } from '@nestjs/typeorm';
+import { Administrador } from 'src/entities/administrador.entity';
+import { Repository } from 'typeorm';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+  OnApplicationBootstrap,
+} from '@nestjs/common';
+import { CreateAdministradorDto } from './dto/create-administrador.dto';
 import * as bcrypt from 'bcrypt';
-import { UpdateAdministradorDto } from "./dto/update-administrador.dto"
+import { UpdateAdministradorDto } from './dto/update-administrador.dto';
 
 @Injectable()
-export class AdministradoresService{
-    constructor(
-            @InjectRepository(Administrador)
-            private readonly administradorRepository: Repository<Administrador>){}
-    
-    
-    private validarNombre(nombre: string){
-        if(!nombre || nombre.trim().length === 0 ){
-            throw new BadRequestException("El nombre del administrador no puede estar vacío.");
-        }
+export class AdministradoresService implements OnApplicationBootstrap {
+  constructor(
+    @InjectRepository(Administrador)
+    private readonly administradorRepository: Repository<Administrador>,
+  ) {}
+
+  private validarNombre(nombre: string) {
+    if (!nombre || nombre.trim().length === 0) {
+      throw new BadRequestException(
+        'El nombre del administrador no puede estar vacío.',
+      );
+    }
+  }
+
+  async crearAdministrador(dto: CreateAdministradorDto) {
+    this.validarNombre(dto.nombre);
+
+    const emailExistente = await this.administradorRepository.findOneBy({
+      mail: dto.mail,
+    });
+
+    if (emailExistente) {
+      throw new BadRequestException('El email debe ser unico');
     }
 
-    async crearAdministrador(dto: CreateAdministradorDto){
-        this.validarNombre(dto.nombre)
-        
-        
-        const emailExistente = await this.administradorRepository.findOneBy(
-            {mail : dto.mail}
-        )
+    const passwordHash = await bcrypt.hash(dto.password, 10);
 
-        if(emailExistente){
-            throw new BadRequestException('El email debe ser unico');
-        }
+    const administrador = this.administradorRepository.create({
+      nombre: dto.nombre,
+      mail: dto.mail,
+      password: passwordHash,
+    });
 
-        const passwordHash = await bcrypt.hash(dto.password, 10)
+    const guardado = await this.administradorRepository.save(administrador);
 
-        const administrador = this.administradorRepository.create({
-            nombre: dto.nombre,
-            mail: dto.mail,
-            password: passwordHash
-        });
+    //'any' para que TS nos deje borrar la propiedad sin quejas
+    delete (guardado as any).password;
 
-        const guardado = await this.administradorRepository.save(administrador);
+    //Retornamos el objeto creado, sin exponer innecesariamente la contraseña, aunque este hasheada, ya que el cliente no la necesita.
+    return guardado;
+  }
 
-        //'any' para que TS nos deje borrar la propiedad sin quejas
-        delete (guardado as any).password
+  async cambiarContraseña(id: number, dto: UpdateAdministradorDto) {
+    const administrador = await this.administradorRepository.findOneBy({
+      id_administrador: id,
+    });
 
-        //Retornamos el objeto creado, sin exponer innecesariamente la contraseña, aunque este hasheada, ya que el cliente no la necesita.
-        return guardado;
+    if (!administrador) {
+      throw new BadRequestException('Usuario no encontrado.');
     }
 
-    async cambiarContraseña(id:number ,dto: UpdateAdministradorDto){
-        const administrador = await this.administradorRepository.findOneBy({
-            id_administrador: id
-        });
+    const actualPasswordComparition = await bcrypt.compare(
+      dto.passwordActual,
+      administrador.password,
+    );
 
-        if(!administrador){
-            throw new BadRequestException('Usuario no encontrado.');
-        }
-
-        const actualPasswordComparition = await bcrypt.compare(dto.passwordActual,  administrador.password)
-
-        if(!actualPasswordComparition){
-            throw new BadRequestException('Contraseña incorrecta');
-        }
-
-        if(dto. passwordNueva !== dto.passwordConfirm){
-            throw new BadRequestException('La nueva contraseña no coincide con el campo de confirmacion');
-        }
-
-        const newPasswordHash = await bcrypt.hash(dto.passwordNueva,10)
-
-        return await this.administradorRepository.update(id,{password: newPasswordHash});
+    if (!actualPasswordComparition) {
+      throw new BadRequestException('Contraseña incorrecta');
     }
 
-    async obtenerAdminPorSuEMail(email:string){
-        return await this.administradorRepository.findOne({
-            where: {mail : email}
-        });
+    if (dto.passwordNueva !== dto.passwordConfirm) {
+      throw new BadRequestException(
+        'La nueva contraseña no coincide con el campo de confirmacion',
+      );
     }
 
+    const newPasswordHash = await bcrypt.hash(dto.passwordNueva, 10);
 
-    async obtenerTodos(){
-        const administradores = await this.administradorRepository.find();
-        
-        if (!administradores) {
-            throw new NotFoundException('No hay resultados');
-        }
+    return await this.administradorRepository.update(id, {
+      password: newPasswordHash,
+    });
+  }
 
-        return administradores
+  async obtenerAdminPorSuEMail(email: string) {
+    return await this.administradorRepository.findOne({
+      where: { mail: email },
+    });
+  }
+
+  async obtenerTodos() {
+    const administradores = await this.administradorRepository.find();
+
+    if (!administradores) {
+      throw new NotFoundException('No hay resultados');
     }
 
-    async obtenerPorId(id: number){
-        const administrador = await this.administradorRepository.findOne({
-            where: {id_administrador: id}
-        })
+    return administradores;
+  }
 
-        if(!administrador){
-            throw new NotFoundException('Usuario no encontrado');
-        }
+  async obtenerPorId(id: number) {
+    const administrador = await this.administradorRepository.findOne({
+      where: { id_administrador: id },
+    });
 
-        return administrador;
+    if (!administrador) {
+      throw new NotFoundException('Usuario no encontrado');
     }
-    
+
+    return administrador;
+  }
+
+  async onApplicationBootstrap() {
+    const emailPorDefecto = 'admin@misierra.com'; // O el mail de tu cliente
+
+    // Verificamos si ya existe el usuario (Idempotencia)
+    const adminExistente = await this.administradorRepository.findOne({
+      where: { mail: emailPorDefecto },
+    });
+
+    if (!adminExistente) {
+      console.log(
+        '🌱 No se encontró administrador. Creando usuario inicial...',
+      );
+
+      const hashedPassword = await bcrypt.hash('Admin1234*', 10); // Usa una contraseña provisoria fuerte
+
+      const nuevoAdmin = this.administradorRepository.create({
+        nombre: 'Administrador',
+        mail: emailPorDefecto,
+        password: hashedPassword,
+      });
+
+      await this.administradorRepository.save(nuevoAdmin);
+      console.log('✅ Administrador inicial creado con éxito.');
+    } else {
+      console.log('📋 El administrador inicial ya existe. Saltando seeding.');
+    }
+  }
 }
